3be1fdfb592828a9503d238f77610bce38cb79d0
Anna Maresova <anicka@suse.cz>
2023-02-03 20:15:47 +0100

    add postprocessing heuristics to exclude duplicates deliberately exclude all cross- packages

--- crunch.pl
+++ crunch.pl
@@ -7,33 +7,44 @@
 # input is expected on STDIN
 
 use strict;
+use Digest::SHA qw(sha1);
 
 my ($code_old, $diff_old) = (0, 0);
 my ($prefix, $prefix_old) = ("", "");
 my ($name, $code, $diff) = (0, 0, 0);
 my ($sc, $sd) = (0, 0);
+my %hash = ();
 
 while (<STDIN>) {
 	 ($code_old, $diff_old) = ($code, $diff);
 	 $prefix_old = $prefix;
 	 ($name, $code, $diff) = split(" ", $_);
-	 my $shortname = split(".", $name);
-	 my @splitted_name = split("-", $shortname);
+	 my @shortname = split("\\.", $name);
+	 my @splitted_name = split("-", $shortname[0]);
 	 if (defined $splitted_name[1]) { #there is some - in name
 	 	pop(@splitted_name);
          }
+	 
+	 my $prefix1 = $splitted_name[0];
+	 my $sum = Digest::SHA::sha1_hex($prefix1.$code);
+	 
 	 $prefix = join("-", @splitted_name);
-	 #	 print($prefix."\n");
-
-	 if ( !$code || ( !(((abs($code - $code_old) < $code/100)) && ($prefix eq $prefix_old))) ) { #packages are excluded if they have the same prefix and codesize differs just a little
-	 # 	 	print("+");
-	 #	 	print($name," ",$code, " ", $diff, "\n");
+	 if ($shortname[0] eq "kernel-source") {
+		 $prefix = "kernel-source";
+	 }
+	#packages are excluded if they have the same first prefix and  exactly the same size
+	#we also exclude copies of gcc
+	#we try to exclude kernel packages that differ just a little
+	 if ( !$code || ( (!(((abs($code - $code_old) < $code/100)) && ($prefix eq $prefix_old))) && ($prefix1 ne "cross") && !defined($hash{$sum})) ){ 
+		 # print("+");
+			print($name," ",$code, " ", $diff, "\n");
 			$sc += $code;
 			$sd += $diff;
 	}
-	# else {
-	#	print("-",$name," ",$code, " ", $diff, "\n");
-	#}
+	#		else {
+			#		print("-",$name," ",$code, " ", $diff, "\n");
+			#	}
+	$hash{$sum} = 1;
 }
 
 print ($sc, " ", $sd, "\n");
