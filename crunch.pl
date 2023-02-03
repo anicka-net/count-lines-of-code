b891d99573f078e7ba5b7ce2676fe3dc8a3572ae
Anna Maresova <anicka@suse.cz>
2023-02-03 15:08:03 +0100

    allow 1 percent difference of codesize when excluding the same package (mostly because of kernel)

--- crunch.pl
+++ crunch.pl
@@ -1,28 +1,39 @@
 #!/usr/bin/perl -w
 
 # count-parallell postprocessing
-# This script reads output of count-parallel.py and removes apparent duplicities arising from
-# different fkavours of RPMs containing the same sources
+# This script reads sorted output of count-parallel.py and removes apparent duplicities arising from
+# different flavours of RPMs containing the same sources
 
 # input is expected on STDIN
 
 use strict;
 
 my ($code_old, $diff_old) = (0, 0);
+my ($prefix, $prefix_old) = ("", "");
 my ($name, $code, $diff) = (0, 0, 0);
 my ($sc, $sd) = (0, 0);
 
 while (<STDIN>) {
 	 ($code_old, $diff_old) = ($code, $diff);
+	 $prefix_old = $prefix;
 	 ($name, $code, $diff) = split(" ", $_);
-	 if ( !$code || (($code != $code_old) || ($diff != $diff_old))) {
-	 #		 	print("+");
-		 	print($name," ",$code, " ", $diff, "\n");
+	 my $shortname = split(".", $name);
+	 my @splitted_name = split("-", $shortname);
+	 if (defined $splitted_name[1]) { #there is some - in name
+	 	pop(@splitted_name);
+         }
+	 $prefix = join("-", @splitted_name);
+	 #	 print($prefix."\n");
+
+	 if ( !$code || ( !(((abs($code - $code_old) < $code/100)) && ($prefix eq $prefix_old))) ) { #packages are excluded if they have the same prefix and codesize differs just a little
+	 # 	 	print("+");
+	 #	 	print($name," ",$code, " ", $diff, "\n");
 			$sc += $code;
 			$sd += $diff;
-	 } #else {
-	 #	print("-",$name," ",$code, " ", $diff, "\n");
-	 #}
+	}
+	# else {
+	#	print("-",$name," ",$code, " ", $diff, "\n");
+	#}
 }
 
 print ($sc, " ", $sd, "\n");
